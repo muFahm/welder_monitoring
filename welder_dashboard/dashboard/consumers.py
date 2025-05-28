3246a4b95fb33f49eb473500bb75c989f44e7294
fahmi <muhfhm@gmail.com>
2025-05-28 08:11:25 +0700

    feat: add new database model to sqlite

--- welder_dashboard/dashboard/consumers.py
+++ welder_dashboard/dashboard/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import SensorRecord
+from datetime import datetime
+from asgiref.sync import sync_to_async
 
 class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,5 +16,29 @@ class DataConsumer(AsyncWebsocketConsumer):
         try:
             data = json.loads(text_data)
             print("Received from ESP:", data)
+            
+            # Parsing timestatmp yang salah format
+            try:
+                timestamp = datetime.fromisoformat(data["timestamp"])
+            except Exception:
+                timestamp = datetime.now()
+
+            # Save data to database async
+            await self.save_sensor_data (data, timestamp)
         except json.JSONDecodeError:
             print("Invalid JSON received")
+
+    @sync_to_async
+    def save_sensor_data(self, data, timestamp):
+        SensorRecord.objects.create(
+            gx = data.get ("gx", 0),
+            gy = data.get ("gy", 0),
+            gz = data.get ("gz", 0),
+            ax = data.get ("ax", 0.0),
+            ay = data.get ("ay", 0.0),
+            az = data.get ("az", 0.0),
+            mx = data.get ("mx", 0.0),
+            my = data.get ("my", 0.0),
+            mz = data.get ("mz", 0.0),
+            timestamp = timestamp,
+        )
--- welder_dashboard/dashboard/models.py
+++ welder_dashboard/dashboard/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class SensorRecord(models.Model):
+    gx = models.IntegerField()
+    gy = models.IntegerField()
+    gz = models.IntegerField()
+    
+    ax = models.FloatField()
+    ay = models.FloatField()
+    az = models.FloatField()
+    
+    mx = models.FloatField()
+    my = models.FloatField()
+    mz = models.FloatField()
+    
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+        ]
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"SensorRecord at {self.timestamp}"
