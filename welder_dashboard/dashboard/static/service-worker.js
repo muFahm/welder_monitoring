c47e65634d0ac4542709503e115660046084e661
fahmi <muhfhm@gmail.com>
2025-09-12 06:24:41 +0700

    Add PWA support with manifest, service worker, and icons; remove deprecated index-proto.html

---
+++ welder_dashboard/dashboard/static/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'welder-monitoring-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png',
+  // Tambahkan asset lain jika perlu
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
